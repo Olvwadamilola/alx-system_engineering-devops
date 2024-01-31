9b71666fec33176ab2d559c20d010e74796c1411
olvvvadamilola <destric3@gmail.com>
2024-01-31 20:39:42 +0100

    check

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
-#regular expression that will match hbtn
+
+# Check if an argument is provided
 if ARGV.empty?
     puts "Please provide an argument."
   else
     input_string = ARGV[0]
   
-    # Regular expression matching
-    match = input_string.scan(/hbt{2,5}n/).flatten
+    # Regular expression matching for the provided pattern
+    match = input_string.scan(/hbtttn+/).flatten
   
     # Output the result
     if match.any?
-        puts match.join
+      puts match.join
     else
-      puts ""
+      puts "No match for the pattern in the provided string."
     end
   end
+  
