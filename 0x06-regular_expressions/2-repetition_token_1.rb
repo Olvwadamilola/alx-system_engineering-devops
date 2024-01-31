4bd9027fd8c4275bd952c1acef2feb896e167527
olvvvadamilola <destric3@gmail.com>
2024-01-31 20:42:41 +0100

    check

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+#regular expression that will match hbtn
+if ARGV.empty?
+    puts "Please provide an argument."
+  else
+    input_string = ARGV[0]
+  
+    # Regular expression matching
+    match = input_string.scan(/hb?tn/).flatten
+  
+    # Output the result
+    if match.any?
+        puts match.join
+    else
+      puts ""
+    end
+  end
+  
