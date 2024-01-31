a998065f0a74a52c0e305a3c71c14e8b03ded6f9
olvvvadamilola <destric3@gmail.com>
2024-01-31 20:36:40 +0100

    check

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,17 +2,17 @@
 
 # Check if an argument is provided
 if ARGV.empty?
-        puts "Please provide an argument."
-    else
-        input_string = ARGV[0]
-    
-        # Regular expression matching
-        match = input_string.scan(/hbt{2,5}n/).flatten
-    
-        # Output the result
-        if match.any?
-            puts match.join
-        else
-            puts ""
-        end
-    end
+puts "Please provide an argument."
+else
+input_string = ARGV[0]
+
+# Regular expression matching
+match = input_string.scan(/hbt{2,5}n/).flatten
+
+# Output the result
+if match.any?
+puts match.join
+else
+puts ""
+end
+end
